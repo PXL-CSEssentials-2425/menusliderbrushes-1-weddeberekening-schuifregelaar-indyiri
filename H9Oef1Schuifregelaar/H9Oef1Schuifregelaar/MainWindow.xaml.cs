f6a5a9cde8af01baa94f2bd79f07089193c260ae
Indy Iri <indy.iri@student.pxl.be>
2024-11-12 22:25:36 +0100

    Afgewerkt, behalve de 'tooltip', aangezien het onduidelijk is wat er juist in moet komen

--- H9Oef1Schuifregelaar/H9Oef1Schuifregelaar/MainWindow.xaml.cs
+++ H9Oef1Schuifregelaar/H9Oef1Schuifregelaar/MainWindow.xaml.cs
@@ -20,5 +20,29 @@ namespace H9Oef1Schuifregelaar
         {
             InitializeComponent();
         }
+
+        private void salarySlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            double taxes;
+            string formattedTaxes;
+
+            if (salarySlider.Value >= 0 && salarySlider.Value < 10000)
+            {
+                taxes = 0;
+                taxesLabel.Content = $"€ {taxes}";
+            }
+            else if (salarySlider.Value >= 10000 && salarySlider.Value < 50000)
+            {
+                taxes = Math.Round(salarySlider.Value * 0.2, 2);
+                formattedTaxes = taxes.ToString("N2");                          // dit zet het in de notatie met een punt tussen elke 3 nullen, en gaat tot twee cijfers na de komma
+                taxesLabel.Content = $"€ {formattedTaxes}";
+            }
+            else if (salarySlider.Value >= 50000 && salarySlider.Value <= 100000)
+            {
+                taxes = Math.Round(salarySlider.Value * 0.9,2);
+                formattedTaxes = taxes.ToString("N2");
+                taxesLabel.Content = $"€ {formattedTaxes}";
+            }
+        }
     }
 }
